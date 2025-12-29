eb189a5d79f403d272ed8eae687f4ce971955907
Sam Wilson <storypixel@gmail.com>
2025-12-28 20:02:59 -0600

    Update dependencies and build tooling

--- dist/scripts/all.js
+++ dist/scripts/all.js
@@ -1,40 +1,40 @@
 "use strict";
 
 // Wait until stuff loads
+
 function init() {
   organizeThoughts();
   initScrollTasks();
-} // Set up scrolling action
+}
 
-
+// Set up scrolling action
 function initScrollTasks() {
   var last_known_scroll_position = 0;
   var ticking = false;
   var sections = document.querySelectorAll(".thought-wrapper");
   var thoughts = document.querySelectorAll(".thought");
   var activeNodes = [];
-
   function processActives() {}
-
   function updateProgress(scroll_pos) {
     var scrolledPast = Array.from(sections).filter(function (el) {
       return el.getBoundingClientRect().top <= 0;
     });
     var activeThoughtWrapper = scrolledPast[scrolledPast.length - 1];
-
     if (activeThoughtWrapper) {
       var topOfActive = activeThoughtWrapper.getBoundingClientRect().top;
       document.getElementById('progress').style.width = 100 * -topOfActive / activeThoughtWrapper.offsetHeight + 'vw';
     }
   }
-
   function watchThoughts(scroll_pos) {
     var className = "active";
     var scrolledPast = Array.from(thoughts).forEach(function (el) {
       var d = document.documentElement;
       var offset = d.scrollTop + window.innerHeight;
-      var height = d.offsetHeight; // console.log('offset = ' + offset);
+      var height = d.offsetHeight;
+
+      // console.log('offset = ' + offset);
       // console.log('height = ' + height);
+
       // if (offset === height) {
       //   console.log('At the bottom');
       // }
@@ -45,10 +45,8 @@ function initScrollTasks() {
       }
     });
   }
-
   window.addEventListener('scroll', function (e) {
     last_known_scroll_position = window.scrollY;
-
     if (!ticking) {
       window.requestAnimationFrame(function () {
         updateProgress(last_known_scroll_position);
@@ -59,9 +57,9 @@ function initScrollTasks() {
     }
   });
   watchThoughts(0);
-} // Put color classes on the .thought items. Color classes gen'd by SCSS
+}
 
-
+// Put color classes on the .thought items. Color classes gen'd by SCSS
 function organizeThoughts() {
   var className = "thought";
   var elements = document.querySelectorAll(".thought");
@@ -70,7 +68,6 @@ function organizeThoughts() {
     if (el.classList) el.classList.add(thisClassName);else el.className += " " + thisClassName;
   });
 }
-
 window.onload = function () {
   init();
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,210 +1,92 @@
 /**
- * This example:
- *  Uses the built-in BrowserSync server for HTML files
- *  Watches & compiles SASS files
- *  Watches & injects CSS files
+ * Gulp build for storypixel.github.io
+ *  - BrowserSync dev server
+ *  - SASS compilation
+ *  - ES6 transpilation via Babel
  */
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var filter = require('gulp-filter');
-
 var clean = require('gulp-clean');
-var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin');
 var sourcemaps = require('gulp-sourcemaps');
 var babel = require('gulp-babel');
-var clean = require('gulp-clean');
 
 var bases = {
   app: './',
   dist: 'dist/',
 };
 
-// var paths = {
-//  scripts: ['scripts/**/*.js', '!scripts/libs/**/*.js'],
-//  libs: ['scripts/libs/jquery/dist/jquery.js', 'scripts/libs/underscore/underscore.js', 'scripts/backbone/backbone.js'],
-//  styles: ['styles/**/*.css'],
-//  html: ['index.html', '404.html', '*.html'],
-//  images: ['images/**/*.png'],
-//  extras: ['crossdomain.xml', 'humans.txt', 'manifest.appcache', 'robots.txt', 'favicon.ico'],
-// };
-
 var paths = {
-  scripts: [
-    'scripts/**/*.js',
-    /*'node_modules/scrollsnap-polyfill/dist/scrollsnap-polyfill.bundled.js',*/ '!scripts/libs/**/*.js',
-  ],
-  libs: [
-    /*'node_modules/scrollsnap-polyfill/dist/scrollsnap-polyfill.js'*/
-  ],
+  scripts: ['scripts/**/*.js', '!scripts/libs/**/*.js'],
+  libs: [],
   styles: ['styles/**/*.css'],
   scss: ['scss/**/*.scss', 'scss/*.scss'],
   html: ['index.html', '404.html'],
   images: ['images/**/*.png', 'images/**/*.svg'],
-  extras: [
-    'crossdomain.xml',
-    'humans.txt',
-    'manifest.appcache',
-    'robots.txt',
-    'favicon.ico',
-  ],
+  extras: ['crossdomain.xml', 'humans.txt', 'manifest.appcache', 'robots.txt', 'favicon.ico'],
 };
 
 // Delete the dist directory
 gulp.task('clean', function() {
-  return gulp.src(bases.dist).pipe(clean());
+  return gulp.src(bases.dist, { read: false, allowEmpty: true }).pipe(clean());
 });
 
-// Process scripts and concatenate them into one output file
-// gulp.task('scripts', ['clean'], function() {
-gulp.task('scripts', gulp.series('clean'), function() {
-  gulp
-    .src(paths.scripts, { cwd: bases.app })
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(uglify())
-    .pipe(concat('app.min.js'))
-    .pipe(gulp.dest(bases.dist + 'scripts'));
+// Copy files to dist
+gulp.task('copy', function() {
+  gulp.src(paths.images, { cwd: bases.app }).pipe(gulp.dest(bases.dist + 'images'));
+  return gulp.src(paths.extras, { cwd: bases.app, allowEmpty: true }).pipe(gulp.dest(bases.dist));
 });
 
-// gulp.task('a', ['b', 'c'], function () { // do something })
-// To convert this for gulp@4.0
-// gulp.task('a', gulp.series(gulp.parallel('b', 'c'), function () {
-//   // do something
-// }))
-
-// Imagemin images and ouput them in dist
-// gulp.task('imagemin', ['clean'], function() {
-gulp.task(
-  'imagemin',
-  gulp.series(gulp.series('clean'), function() {
-    gulp
-      .src(paths.images, { cwd: bases.app })
-      .pipe(imagemin())
-      .pipe(gulp.dest(bases.dist + 'images/'));
-  })
-);
-
-// Copy all other files to dist directly
-// gulp.task('copy', ['clean'], function() {
-gulp.task(
-  'copy',
-  gulp.series(gulp.series('clean'), function() {
-    // Copy html
-    // gulp.src(paths.html, {cwd: bases.app})
-    // .pipe(gulp.dest(bases.dist));
-
-    // Copy styles
-    // gulp.src(paths.styles, {cwd: bases.app})
-    // .pipe(gulp.dest(bases.dist + 'styles'));
-
-    // Copy lib scripts, maintaining the original directory structure
-    // gulp.src(paths.libs, {cwd: 'app/**'})
-    // .pipe(gulp.dest(bases.dist));
-
-    gulp
-      .src(paths.images, { cwd: bases.app })
-      .pipe(gulp.dest(bases.dist + 'images'));
-    // Copy extra html5bp files
-    gulp.src(paths.extras, { cwd: bases.app }).pipe(gulp.dest(bases.dist));
-  })
-);
-
-// A development task to run anytime a file changes
-gulp.task('watch', function() {
-  gulp.watch(bases.app, gulp.parallel('scripts', 'copy'));
-});
-
-// Define the default task as a sequence of the above tasks
-// gulp.task('default', ['clean', 'scripts', 'imagemin', 'copy']);
-
-// Browser-sync task, only cares about compiled CSS
+// Browser-sync dev server
 gulp.task('browser-sync', function() {
   browserSync({
     notify: true,
-    //proxy: "http://localhost:3000",
     server: {
       baseDir: bases.app,
-      injectChanges: true, // this is new
+      injectChanges: true,
     },
     port: 3000,
-
-    // ui: {
-    //   port: 6969
-    // },
-    // port: 6968
   });
 });
 
 gulp.task('html', function() {
-  return gulp.src(paths.html).pipe(reload({ stream: true })); // inject into browsers
+  return gulp.src(paths.html).pipe(reload({ stream: true }));
 });
 
-// Sass task, will run when any SCSS files change.
+// Compile SASS
 gulp.task('styles', function() {
   return gulp
     .src('./scss/style.scss')
-    .pipe(sass({ includePaths: ['scss'] })) // compile sass
-    .pipe(gulp.dest(bases.dist + 'styles')) // write to css dir
-    .pipe(reload({ stream: true })); // inject into browsers
+    .pipe(sass({ includePaths: ['scss'] }).on('error', sass.logError))
+    .pipe(gulp.dest(bases.dist + 'styles'))
+    .pipe(reload({ stream: true }));
 });
 
+// Transpile ES6 and concatenate
 gulp.task('es6', function() {
   return gulp
     .src(paths.scripts)
     .pipe(sourcemaps.init())
-    .pipe(
-      babel({
-        presets: ['@babel/env'],
-      })
-    )
-
+    .pipe(babel({ presets: ['@babel/env'] }))
     .pipe(concat('all.js'))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(bases.dist + 'scripts'))
     .pipe(reload({ stream: true }));
 });
 
-const image = require('gulp-image');
-
-gulp.task('image', function() {
-  gulp
-    .src(paths.images)
-    .pipe(image())
-    .pipe(gulp.dest(bases.dist + 'images'))
-    .pipe(reload({ stream: true })); // inject into browsers
-});
-
-// Delete the dist directory
-gulp.task('clean', function() {
-  return gulp.src(bases.dist).pipe(clean());
-});
-
-// Default task to be run with `gulp`
-// gulp.task('default', ['styles', 'browser-sync'], function () {
-//   gulp.watch(paths.scss, ['styles']);
-//   gulp.watch(paths.html, ['html']);
-//   gulp.watch(paths.scripts, ['es6']);
-//   gulp.watch(paths.images, ['image']);
-// });
-
-// Default task to be run with `gulp`
+// Default task with watch
 gulp.task(
   'default',
-  gulp.series(gulp.parallel('styles', 'browser-sync'), function() {
-    gulp.watch(paths.scss, ['styles']);
-    gulp.watch(paths.html, ['html']);
-    gulp.watch(paths.scripts, ['es6']);
-    gulp.watch(paths.images, ['image']);
+  gulp.series(gulp.parallel('styles', 'browser-sync'), function watchFiles() {
+    gulp.watch(paths.scss, gulp.series('styles'));
+    gulp.watch(paths.html, gulp.series('html'));
+    gulp.watch(paths.scripts, gulp.series('es6'));
   })
 );
 
-// gulp.task('a', ['b', 'c'], function () { // do something })
-// // To convert this for gulp@4.0
-// gulp.task('a', gulp.series(gulp.parallel('b', 'c'), function () {
-//   // do something
-// }))
+// Build task
+gulp.task('build', gulp.series('clean', gulp.parallel('styles', 'es6', 'copy')));
